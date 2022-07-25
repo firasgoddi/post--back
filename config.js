9bf8bee500ae44ca90c6d38bff6a0e925e12d73b
goddi.firas94@gmail.com <goddi.firas94@gmail.com>
2022-07-25 19:27:46 +0100

    post

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports ={
+    MONGODB     :'mongodb+srv://classed0:aeQsAzW6ikzOexUJ@cluster0.skkli.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    SECRET_KEY :'my secret Place'
+}
+
+
+
